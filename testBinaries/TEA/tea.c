b651607eae75e968a48cf9f28f7242461b811410
j04n.calvet@gmail.com <j04n.calvet@gmail.com@1af76c6a-b2cc-4384-1bb9-bea9cc455f10>
2012-08-31 13:38:33 +0000

    git-svn-id: http://aligot.googlecode.com/svn/trunk/vanilla@23 1af76c6a-b2cc-4384-1bb9-bea9cc455f10

---
+++ testBinaries/TEA/tea.c
@@ -0,0 +1,83 @@
+#include "stdint.h"
+#include <iostream> 
+using namespace std;
+
+ 
+void encrypt (uint32_t* v, uint32_t* k) {
+    uint32_t v0=v[0], v1=v[1], sum=0, i;           /* set up */
+    uint32_t delta=0x9e3779b9;                     /* a key schedule constant */
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* cache key */
+    for (i=0; i < 32; i++) {                       /* basic cycle start */
+        sum += delta;
+        v0 += ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        v1 += ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+    }                                              /* end cycle */
+    v[0]=v0; v[1]=v1;
+}
+ 
+void decrypt (uint32_t* v, uint32_t* k) {
+    uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;       /* set up */
+    uint32_t delta=0x9e3779b9;                          /* a key schedule constant */
+    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];        /* cache key */
+    for (i=0; i<32; i++) {                              /* basic cycle start */
+        v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
+        v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
+        sum -= delta;                                   
+    }                                                   /* end cycle */
+    v[0]=v0; v[1]=v1;
+}
+
+void decode(long* v, long* k)
+{
+    unsigned long n=16,sum,y=v[0],z=v[1],delta=0x12345678;
+    sum = delta << 4;
+    while(n-->0)
+    {
+        z-= ((y << 4) + k[2]) ^ (y+sum) ^ ((y>>5) + k[3]);
+        y-= ((z<<4) + k[0]) ^ (z+sum) ^ ((z>>5) + k[1]);
+        sum-=delta;
+    }
+    v[0]=y;
+    v[1]=z;
+}
+
+int main()
+{
+    
+    uint32_t v1[2];
+    v1[0] = 0x01234567;
+    v1[1] = 0x89ABCDEF;
+
+    uint32_t key[4];
+    key[0] = 0xDEADBEE1;
+    key[1] = 0xDEADBEE2;
+    key[2] = 0xDEADBEE3;
+    key[3] = 0xDEADBEE4;
+
+    decrypt(v1, key);
+
+    cout << hex << v1[0] << endl;
+    cout << hex << v1[1] << endl;
+    
+
+    // Original implementation
+    /*long v_bis[2];
+    long key_bis[4];
+
+    v_bis[0] = 0x01234567;
+    v_bis[1] = 0x89ABCDEF;
+    
+    key_bis[0] = 0xDEADBEE1;
+    key_bis[1] = 0xDEADBEE2;
+    key_bis[2] = 0xDEADBEE3;
+    key_bis[3] = 0xDEADBEE4;
+
+    decode(v_bis,key_bis);
+    
+    cout << hex << v_bis[0] << endl;
+    cout << hex << v_bis[1] << endl;*/
+
+    return 0;
+
+    
+}
